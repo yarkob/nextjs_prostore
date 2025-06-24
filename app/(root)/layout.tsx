287ad53a8970bb92c7264e7c2357f83a288b10d3
yarkob <yaroslav.klark.bondarenko@gmail.com>
2025-06-24 10:59:54 +0300

    Add header and footer

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/shared/header";
+import Footer from "@/components/footer";
 
 export default function RootLayout({
   children,
@@ -11,6 +12,7 @@ export default function RootLayout({
       <main className="flex-1 wrapper">
         {children}
       </main>
+      <Footer />
     </div>
   );
 }
--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="flex-start">
         <Link href='/' className="flex-start">
           <Image
-            src='/images/logo.png'
+            src='/images/logo.svg'
             alt={`${APP_NAME} logo`}
             height={48}
             width={48}
