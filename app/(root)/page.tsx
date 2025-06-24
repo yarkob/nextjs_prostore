719389aa3b893f975c4e8336e22e1d94ea135be7
yarkob <yaroslav.klark.bondarenko@gmail.com>
2025-06-24 13:43:32 +0300

    add theme toggle, loading, not found pages, sheet menu, product list

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,10 @@
-export default function Home() {
-  return <>Prostore</>;
+import sampleData from "@/db/sample-data";
+import ProductList from "@/components/shared/product/product-list";
+
+const Homepage = () => {
+  return <>
+    <ProductList data={sampleData.products} title='Newest Arrivals' limit={4} />
+  </>;
 }
+
+export default Homepage;
--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,8 +1,7 @@
-import { ShoppingCart, UserIcon } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/constants";
+import Menu from "./menu";
 
 const Header = () => {
   return <header className='w-full border-b'>
@@ -21,18 +20,7 @@ const Header = () => {
           </span>
         </Link>
       </div>
-      <div className='space-x-2'>
-        <Button asChild variant='ghost'>
-          <Link href='/cart'>
-            <ShoppingCart /> Cart
-          </Link>
-        </Button>
-        <Button asChild variant='ghost'>
-          <Link href='/sign-in'>
-            <UserIcon /> Sign In
-          </Link>
-        </Button>
-      </div>
+      <Menu />
     </div>
   </header>;
 }
