a8febbdfa14e3c1cc0bf61f50d4897560118893d
yarkob <yaroslav.klark.bondarenko@gmail.com>
2025-07-03 12:34:35 +0300

    add images to product page

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -4,6 +4,7 @@ import ProductPrice from "@/components/shared/product/product-price";
 import {Card, CardContent} from "@/components/ui/card";
 import {Badge} from "@/components/ui/badge";
 import {Button} from "@/components/ui/button";
+import ProductImages from "@/components/shared/product/product-images";
 
 const ProductDetailsPage = async (props: {
   params: Promise<{ slug: string }>
@@ -17,7 +18,9 @@ const ProductDetailsPage = async (props: {
     <section>
       <div className="grid grid-cols-1 md:grid-cols-5">
         {/* Images Column */}
-        <div className="col-span-2">{/* Images Component */}</div>
+        <div className="col-span-2">
+          <ProductImages images={product.images} />
+        </div>
         {/* Details Column */}
         <div className="col-span-2 p-5">
           <div className="flex flex-col gap-6">
